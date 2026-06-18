dcc5fe0378ad9f5daf8689ca753a33c33fe90f7d
newone0707 <yfree12324@gmail.com>
2026-06-18 13:46:15 +0530

    fix: config.py

--- config.py
+++ config.py
@@ -8,7 +8,7 @@ API_ID = int(os.environ.get("API_ID", "1234567"))
 API_HASH = os.environ.get("API_HASH", "your_api_hash_here")
 
 # Bot Token from @BotFather
-BOT_TOKEN = os.getenv("BOT_TOKEN", "8762166731:AAEUa-CEhKRLsfWGDdSk4Gau_ujJbMHgcFs")
+BOT_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN") or os.getenv("BOT_TOKEN", "8762166731:AAEUa-CEhKRLsfWGDdSk4Gau_ujJbMHgcFs")
 
 # Owner ID and Authorized Users
 OWNER_ID = int(os.environ.get("OWNER_ID", "123456789"))
